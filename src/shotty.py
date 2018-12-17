08eb3dad46cde7eb52a86d31f05c678555a82ae3
Ian Johnson <tacoda@pm.me>
2018-12-16 21:44:28 -0500

    Add instance stop command

--- src/shotty.py
+++ src/shotty.py
@@ -42,5 +42,23 @@ def list_instances(project):
 
     return
 
+@instances.command('stop')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def stop_instances(project):
+    "Stop EC2 instances"
+    instances = []
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+
+    return
+
 if __name__ == '__main__':
     instances()
