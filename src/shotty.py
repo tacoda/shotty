e14c5e3ef66d2e1fc9b09090931a9306f3092de7
Ian Johnson <tacoda@pm.me>
2018-12-16 23:37:39 -0500

    Add volumes list subcommand

--- src/shotty.py
+++ src/shotty.py
@@ -33,7 +33,22 @@ def cli():
 def volumes():
     """Commands for volumes"""
 
+@volumes.command('list')
+@click.option('--project', default=None,
+    help="Only volumes for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 volumes"
+    instances = filter_instances(project)
+    for i in instances:
+        for v in i.volumes.all():
+            print(", ".join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted")))
 
+    return
 
 @cli.group('instances')
 def instances():
