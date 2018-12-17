fdbf0a0c1126ba4096c011ce3b4f60e96a0c6b65
Ian Johnson <tacoda@pm.me>
2018-12-17 08:50:33 -0500

    Add error handling for instance state when starting and stopping instances

--- src/shotty.py
+++ src/shotty.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import boto3
+import botocore
 import click
 
 # TODO: Replace env with yaml
@@ -124,7 +125,11 @@ def stop_instances(project):
     instances = filter_instances(project)
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Could not stop {0}:".format(i.id))
+            print(str(e))
 
     return
 
@@ -136,7 +141,11 @@ def start_instances(project):
     instances = filter_instances(project)
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Could not start {0}:".format(i.id))
+            print(str(e))
 
     return
 
